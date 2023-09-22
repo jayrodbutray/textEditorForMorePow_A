3902fced24d13057e4e747f13d9064015a741c97
Jared Buttrey <geraldomania@hotmail.com>
2023-09-21 21:19:04 -0700

    database changes

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,23 +14,24 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const db = await initdb();
-  const tx = db.transaction('jate', 'readwrite');
+  const jateDb = await openDB('jate_db', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  await store.add(content);
-  await tx.done;
-  console.log('Content added to the database:', content);
+  const request = store.put({id: 1, value: content});
+  const result = await request;
+  console.log('Content added to the database:', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const db = await initdb();
-  const tx = db.transaction('jate', 'readonly');
+  const jateDb = await openDB('jate_db', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const allContent = await store.getAll();
-  await tx.done;
-  console.log('All content from the database:', allContent);
-  return allContent;
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+
+  return result;
 };
 //comments to see if i can push changes finally
 initdb();
